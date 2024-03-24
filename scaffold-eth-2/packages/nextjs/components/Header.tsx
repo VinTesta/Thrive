14b8ec36089411c0b54bbcfa69a4fbc24c2d9c22
LeoAndriolo <leonardo.andriolo@sou.inteli.edu.br>
2024-03-24 02:35:19 -0300

    Atualiza elementos do guildbuild

--- scaffold-eth-2/packages/nextjs/components/Header.tsx
+++ scaffold-eth-2/packages/nextjs/components/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useCallback, useRef, useState } from "react";
-import Image from "next/image";
+import headerImage from '../components/assets/thriveArt2.png';
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
@@ -90,11 +90,11 @@ export const Header = () => {
         </div>
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
           <div className="flex relative w-10 h-10">
-            <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
+            <img alt="Thrive logo" className="cursor-pointer" fill src={headerImage} />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">Thrive</span>
+            <span className="text-xs">Scholarship program</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
